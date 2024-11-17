85b99bb791c37c9e2f14f707e2400fc4c2d47d0e
Yasin Gunaydin <81875281+yasingunaydiin@users.noreply.github.com>
2024-11-18 00:13:40 +0300

    Updated portfolio

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,20 +1,20 @@
 'use client';
-import Image from 'next/image';
-import { Card } from '@/components/Card';
-import HTML5Icon from '@/assets/icons/HTML5_Badge.png';
 import CSSIcon from '@/assets/icons/CSS3_Badge.png';
-import JSIcon from '@/assets/icons/Javascript_Badge.png';
-import ReactIcon from '@/assets/icons/React_Badge.png';
-import NextJSIcon from '@/assets/icons/NextJS_Badge.png';
-import TailwindIcon from '@/assets/icons/Tailwind_Badge.png';
-import PythonIcon from '@/assets/icons/Python_Badge.png';
-import SQLIcon from '@/assets/icons/SQL_Icon.png';
 import GitIcon from '@/assets/icons/Git_Badge.png';
-import mapImage from '@/assets/images/map.png';
+import HTML5Icon from '@/assets/icons/HTML5_Badge.png';
+import JSIcon from '@/assets/icons/Javascript_Badge.png';
+import NextJSIcon from '@/assets/icons/NextJS_Badge.png';
+import PythonIcon from '@/assets/icons/Python_Badge.png';
+import ReactIcon from '@/assets/icons/React_Badge.png';
+import SQLIcon from '@/assets/icons/SQL_Icon.png';
+import TailwindIcon from '@/assets/icons/Tailwind_Badge.png';
 import faceMemoji from '@/assets/images/memoji-face.png';
+import theNetherlandsMapImage from '@/assets/images/the-netherlands-map.png';
+import turkeyMapImage from '@/assets/images/turkey-map.png';
+import { Card } from '@/components/Card';
 import { CardHeader } from '@/components/CardHeader';
 import { ToolboxItems } from '@/components/ToolboxItems';
-import { motion } from 'framer-motion';
+import Image from 'next/image';
 import { useRef } from 'react';
 
 const toolboxItems = [
@@ -56,68 +56,101 @@ const toolboxItems = [
   },
 ];
 
-const hobbies = [
-  {
-    title: 'Photo Editing',
-    emoji: '📷',
-    left: '5%',
-    top: '5%',
-  },
-  {
-    title: 'Video Editing',
-    emoji: '📹',
-    left: '45%',
-    top: '55%',
-  },
-  {
-    title: 'Gaming',
-    emoji: '🎮',
-    left: '10%',
-    top: '35%',
-  },
-  {
-    title: 'Traveling',
-    emoji: '🚞',
-    left: '50%',
-    top: '30%',
-  },
-  {
-    title: 'Running',
-    emoji: '🏃',
-    left: '5%',
-    top: '65%',
-  },
-];
+// const hobbies = [
+//   {
+//     title: 'Photo Editing',
+//     emoji: '📷',
+//     left: '5%',
+//     top: '5%',
+//   },
+//   {
+//     title: 'Video Editing',
+//     emoji: '📹',
+//     left: '45%',
+//     top: '55%',
+//   },
+//   {
+//     title: 'Gaming',
+//     emoji: '🎮',
+//     left: '10%',
+//     top: '35%',
+//   },
+//   {
+//     title: 'Traveling',
+//     emoji: '🚞',
+//     left: '50%',
+//     top: '30%',
+//   },
+//   {
+//     title: 'Running',
+//     emoji: '🏃',
+//     left: '5%',
+//     top: '65%',
+//   },
+// ];
 
 export const AboutSection = () => {
   const constraintRef = useRef(null);
   return (
-    <div className="py-20">
-      <div className="container">
-        <h2 className="text-3xl text-center md:text-5xl mt-6">
-          A Little About Me
-        </h2>
-        <p className="text-base text-center md:text-2xl mt-3">Who am I?</p>
-        <div className="mt-16 flex flex-col gap-6">
-          <Card className="h-[320px] p-0">
+    <div className='py-20'>
+      <div className='container'>
+        <h2 className='text-3xl text-center md:text-5xl'>A Little About Me</h2>
+        <p className='text-base text-center md:text-2xl mt-3'>Who am I?</p>
+        <div className='mt-10 flex flex-col gap-6'>
+          <Card className='h-[320px] p-0'>
             <CardHeader
-              title="My Toolbox"
-              description="Explore the technologies and tools I use to create projects."
-              className="px-6 pt-6"
+              title='My Toolbox'
+              description='Explore the technologies and tools I use to create projects.'
+              className='px-6 pt-6'
             />
             <ToolboxItems
               items={toolboxItems}
-              className="mt-6"
-              itemsWrapperClassName="animate-move-left"
+              className='mt-6'
+              itemsWrapperClassName='animate-move-left'
             />
             <ToolboxItems
               items={toolboxItems}
-              className="mt-6"
-              itemsWrapperClassName="animate-move-right"
+              className='mt-6'
+              itemsWrapperClassName='animate-move-right'
             />
           </Card>
-          <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
-            <Card className="h-[320px] p-0 flex flex-col col-span-2 md:col-span-3">
+
+          <div className='flex flex-col gap-8'>
+            <Card className='h-[250px] p-0 relative col-span-2'>
+              <Image
+                src={turkeyMapImage}
+                alt='Map'
+                className='h-[340px] lg:h-full lg:w-full object-cover'
+              />
+              <div className="absolute top-[120px] left-[170px] md:top-[70px] md:left-[350px] lg:top-[40px] lg:left-[565px] -translate-x-1/2 -translate-y-1/2 size-11 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-blue-400/50">
+                <div className='absolute inset-0 rounded-full bg-gradient-to-r from-sky-200 to-sky-400 -z-10 animate-ping [animation-duration:1.5s]'></div>
+                <div className='absolute inset-0 rounded-full bg-gradient-to-r from-sky-200 to-sky-400 -z-10'></div>
+                <Image
+                  src={faceMemoji}
+                  alt='Face Memoji'
+                  className='h-11 w-11 object-contain rounded-full'
+                />
+              </div>
+            </Card>
+
+            <Card className='h-[250px] p-0 relative col-span-2'>
+              <Image
+                src={theNetherlandsMapImage}
+                alt='Map'
+                className='h-[380px] lg:h-full lg:w-full object-cover'
+              />
+              <div className="absolute top-[220px] left-[310px] md:top-[150px] md:left-[530px] lg:top-[170px] lg:left-[835px] -translate-x-1/2 -translate-y-1/2 size-11 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-blue-400/50">
+                <div className='absolute inset-0 rounded-full bg-gradient-to-r from-sky-200 to-sky-400 -z-10 animate-ping [animation-duration:1.5s]'></div>
+                <div className='absolute inset-0 rounded-full bg-gradient-to-r from-sky-200 to-sky-400 -z-10'></div>
+                <Image
+                  src={faceMemoji}
+                  alt='Face Memoji'
+                  className='h-11 w-11 object-contain rounded-full'
+                />
+              </div>
+            </Card>
+
+            {/* <Card className="h-[320px] p-0 flex flex-col col-span-2 md:col-span-3">
               <CardHeader
                 title="My Interests"
                 description="Explore my interests and hobbies beyond the code."
@@ -125,12 +158,12 @@ export const AboutSection = () => {
               />
               <div
                 className="relative flex-1"
-                ref={constraintRef} /* To add constraint */
+                ref={constraintRef}
               >
                 {hobbies.map((hobby) => (
                   <motion.div
                     key={hobby.title}
-                    className="inline-flex items-center gap-2 py-1.5 px-6 outline outline-2 outline-black/10 rounded-full absolute bg-white" /* Dragable labels */
+                    className="inline-flex items-center gap-2 py-1.5 px-6 outline outline-2 outline-black/10 rounded-full absolute bg-white"
                     style={{
                       left: hobby.left,
                       top: hobby.top,
@@ -143,24 +176,7 @@ export const AboutSection = () => {
                   </motion.div>
                 ))}
               </div>
-            </Card>
-
-            <Card className="h-[320px] p-0 relative col-span-2">
-              <Image
-                src={mapImage}
-                alt="Map"
-                className="h-full w-full object-cover"
-              />
-              <div className="absolute top-12 left-56 md:top-12 lg:left-72 lg:top-7 md:left-48 -translate-x-1/2 -translate-y-1/2 size-11 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-blue-400/50">
-                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-sky-200 to-sky-400 -z-10 animate-ping [animation-duration:1.5s]"></div>
-                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-sky-200 to-sky-400 -z-10"></div>
-                <Image
-                  src={faceMemoji}
-                  alt="Face Memoji"
-                  className="h-11 w-11 object-contain rounded-full"
-                />
-              </div>
-            </Card>
+            </Card> */}
           </div>
         </div>
       </div>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,12 +1,46 @@
-import progressionDexPage from '@/assets/images/progressiondex-landing-page.png';
-import metaverseNextPage from '@/assets/images/metaverse-next-landing-page.png';
 import illumicheckPage from '@/assets/icons/Illumicheck_mac.png';
 import regexorcistPage from '@/assets/icons/RegExorcist_mac.png';
-import CheckCircleIcon from '@/assets/icons/check-circle.svg';
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
+import CheckCircleIcon from '@/assets/icons/check-circle.svg';
+import actionsListPage from '@/assets/images/actionslist-landing-page.png';
+import metaverseNextPage from '@/assets/images/metaverse-next-landing-page.png';
+import progressionDexPage from '@/assets/images/progressiondex-landing-page.png';
+import yukIlanlariPage from '@/assets/images/yukilanlari-landing-page.png';
 import Image from 'next/image';
 
 const portfolioProjects = [
+  {
+    year: '2024',
+    title: 'Yükilanları.net',
+    results: [
+      {
+        title:
+          'Built with React, Next.js, and Tailwind CSS for responsive design.',
+      },
+      {
+        title: 'Uses MongoDB with Mongoose for data management.',
+      },
+      {
+        title: 'Integrates WorkOS for authentication.',
+      },
+    ],
+    link: 'https://www.yukilanlari.net',
+    image: yukIlanlariPage,
+  },
+  {
+    year: '2024',
+    title: 'actionsList',
+    results: [
+      {
+        title:
+          'Built with Next.js, Shadcn, and Prisma for a seamless CRUD experience.',
+      },
+      { title: 'Validates data using Zod and manages state with SWR.' },
+      { title: 'Stores tasks in a MongoDB database and local storage.' },
+    ],
+    link: 'https://actionslist.vercel.app',
+    image: actionsListPage,
+  },
   {
     year: '2024',
     title: 'Progression Dex',
@@ -55,15 +89,15 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <section className="pb-16 lg:py-28">
-      <div id="projectsSection" className="container">
-        <h2 className="text-3xl text-center md:text-5xl mt-6">
+    <section className='pb-16 lg:py-28'>
+      <div id='projectsSection' className='container'>
+        <h2 className='text-3xl text-center md:text-5xl mt-6'>
           Featured Projects
         </h2>
-        <p className="text-base text-center md:text-2xl mt-3">
+        <p className='text-base text-center md:text-2xl mt-3'>
           Projects I have created to satisfy needs.
         </p>
-        <div className="mt-5 md:mt-10 flex flex-col gap-14">
+        <div className='mt-5 md:mt-10 flex flex-col gap-14'>
           {portfolioProjects.map((project, index) => (
             <div
               key={project.title}
@@ -72,31 +106,31 @@ export const ProjectsSection = () => {
                 top: `calc(64px + ${index * 40}px` /* Stacking cards */,
               }}
             >
-              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-                <div className="lg:pb-16">
-                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+              <div className='lg:grid lg:grid-cols-2 lg:gap-16'>
+                <div className='lg:pb-16'>
+                  <div className='bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text'>
                     <span>{project.year}</span>
                   </div>
-                  <h3 className="text-2xl mt-2 md:text-4xl md:mt-5">
+                  <h3 className='text-2xl mt-2 md:text-4xl md:mt-5'>
                     {project.title}
                   </h3>
-                  <hr className="border-t-2 border-black/5 mt-4" />
-                  <ul className="flex flex-col gap-2 mt-4 md:mt-5">
+                  <hr className='border-t-2 border-black/5 mt-4' />
+                  <ul className='flex flex-col gap-2 mt-4 md:mt-5'>
                     {project.results.map((result) => (
-                      <li className="flex gap-2 text-sm md:text-base">
-                        <CheckCircleIcon className="size-5 md:size-6" />
+                      <li className='flex gap-2 text-sm md:text-base'>
+                        <CheckCircleIcon className='size-5 md:size-6' />
                         <span>{result.title}</span>
                       </li>
                     ))}
                   </ul>
                   <a
                     href={project.link} // Link is dynamically set from project data
-                    target="_blank" // Open in new tab
-                    rel="noopener noreferrer" // Security enhancement
-                    className="border border-black/15 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 md:w-auto md:px-8"
+                    target='_blank' // Open in new tab
+                    rel='noopener noreferrer' // Security enhancement
+                    className='border border-black/15 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 md:w-auto md:px-8'
                   >
                     <span>Visit Live Site</span>
-                    <ArrowUpRightIcon className="size-4" />
+                    <ArrowUpRightIcon className='size-4' />
                   </a>
                 </div>
                 <div>
@@ -104,10 +138,10 @@ export const ProjectsSection = () => {
                     src={project.image}
                     alt={project.title}
                     className={`mt-5 -mb-2 mx-auto md:-mb-5 lg:mt-0 lg:absolute ${
-                      index === 2 || index === 3
+                      index === 4 || index === 5
                         ? 'lg:w-3/5 lg:h-auto lg:relative md:mb-5 mb-5 md:w-3/5 md:h-auto'
                         : 'lg:h-full lg:w-auto'
-                    }`} // index === 2, means illumicheck image, index === 3 means regex image
+                    }`} // index === 4, means images 1 through 4,, index === 5 means the icons
                   />
                 </div>
               </div>
